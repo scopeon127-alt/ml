f4f0917576dba094eff829611dddc4b70c45588c
scopeon127-alt <scopeon127@gmail.com>
2026-02-13 09:04:32 +0530

    added setup.py and requirement.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name ='ML_PROJECT',
+    version='0.0.1',
+    author='Bajrang',
+    author_email='bajranglegha127@gmai.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+    
+)
