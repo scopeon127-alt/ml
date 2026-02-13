90a197feabf85012654885629a65a816ac372ad8
scopeon127-alt <scopeon127@gmail.com>
2026-02-13 09:21:23 +0530

    added requirement function to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
+
+    return requirements
+
 
 setup(
-    name ='ML_PROJECT',
-    version='0.0.1',
-    author='Bajrang',
-    author_email='bajranglegha127@gmai.com',
+    name="ML_PROJECT",
+    version="0.0.1",
+    author="Bajrang",
+    author_email="bajranglegha127@gmail.com",
     packages=find_packages(),
-    install_requires=['pandas','numpy','seaborn']
-    
+    install_requires=get_requirements("requirements.txt")
 )
